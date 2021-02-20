5fe5e9e4033df8c5610c8fa62c35c1d085d78585
Serget <skalytesky@gmail.com>
2021-02-20 13:20:26 +0300

    main tree works

--- main.py
+++ main.py
@@ -1,6 +1,99 @@
-import part
-from bot import dp
+import json
+from bot import dp, bot
+from answers import answer
 from aiogram import executor
+from helpers import prev_path
+from keyboard.appartments import keyboard
+from aiogram.dispatcher import FSMContext
+from aiogram.types import CallbackQuery, Message
+from aiogram.types.reply_keyboard import ReplyKeyboardMarkup
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@dp.message_handler(content_types=['photo'], state='*',)
+async def handle_docs_photo(message: Message, state: FSMContext):
+
+    path = ''
+    async with state.proxy() as data:
+        path = data['path']
+        data['path'] = prev_path(data['path'])
+    path = path.replace('/', '_')
+    await message.photo[-1].download(f'{path}.jpg')
+    await message.reply(data['path'], reply_markup=keyboard[data['path']])
+
+
+@dp.message_handler(state='*', commands=['start', 'help'])
+async def send_welcome(message: Message, state: FSMContext):
+
+    await state.finish()
+
+    async with state.proxy() as data:
+        data['path'] = '/'
+    await message.reply("Введите адрес объекта", reply_markup=keyboard['/'])
+    await message.reply("init", reply_markup=ReplyKeyboardMarkup([['/start'], ['Сохранить'], ['назад']]))
+
+
+@dp.message_handler(lambda message: message.text == 'Сохранить', state='*')
+async def save_progress(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        print(data.__dict__['_copy'])
+        with open('test.json', 'w') as f:
+            json.dump(data.__dict__['_copy'], f)
+    await message.reply('Прогресс сохранен')
+
+
+@dp.message_handler(lambda message: message.text not in ['назад', 'Сохранить', 'Главное меню'], state='*')
+async def value_saver(message: Message, state: FSMContext):
+    path = ''
+    async with state.proxy() as data:
+        data[data['path']] = message.text
+        # data = set_return_path(data)
+        data['path'] = prev_path(data['path'])
+    await message.reply(f'Значение сохраненно {message.text}')
+    await message.reply(data['path'], reply_markup=keyboard[data['path']])
+
+
+@dp.message_handler(lambda message: message.text == 'назад', state='*')
+async def bactrack(message: Message, state: FSMContext):
+    path = ''
+    async with state.proxy() as data:
+        path = prev_path(data['path'])
+        data['path'] = path
+    await message.reply(data['path'], reply_markup=keyboard[path])
+
+
+@dp.callback_query_handler(lambda callback_query: True, state='*')
+async def process_callback(callback_query: CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    path = ''
+    async with state.proxy() as data:
+        path = data['path'] + callback_query.data + '/'
+
+        # check 3 conditions
+        key = '/'.join(data['path'].split('/')[-3:-1])+'/'+callback_query.data
+        if key in answer:
+            data['path'] = path
+            await bot.send_message(callback_query.message.chat.id, answer[key])
+            return
+        # check 2 conditions
+        key = data['path'].split('/')[-2]+'/'+callback_query.data
+        if key in answer:
+            data['path'] = path
+            await bot.send_message(callback_query.message.chat.id, answer[key])
+            return
+        # check 1 condition
+        if callback_query.data in answer:
+            data['path'] = path
+            await bot.send_message(callback_query.message.chat.id, answer[callback_query.data])
+            return
+
+        if path not in keyboard:
+            data[data['path']] = callback_query.data
+            path = prev_path(data['path'])
+            await bot.send_message(callback_query.message.chat.id, f'Значение сохраненно: {callback_query.data}')
+        data['path'] = path
+    await bot.send_message(callback_query.message.chat.id,  data['path'], reply_markup=keyboard[path])
 
 
 if __name__ == '__main__':
