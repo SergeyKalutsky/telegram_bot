a109a3b3c7b5bc928f175610589ddf3520718ffb
Serget <skalytesky@gmail.com>
2021-03-03 15:30:24 +0300

    main loop works as planed

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ from aiogram.types.reply_keyboard import ReplyKeyboardMarkup
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
 
+BASEDIR = 'web/app/app/static/data'
+
 
 @dp.message_handler(content_types=['photo'], state='*',)
 async def handle_docs_photo(message: Message, state: FSMContext):
 
-    img_fname = make_img_fname()
-    img_path = os.path.join('data/photo', img_fname)
-
     async with state.proxy() as data:
+        if 'id' not in data:
+            await message.reply("Не выбран id задачи. Пришлите его в ответ")
+            return
+
+        img_fname = make_img_fname()
+        img_path = os.path.join(BASEDIR, data['id'], 'photo', img_fname)
+
         put_key_json(data['path'], data, img_fname, photo=True)
 
         data['path'] = prev_path(data['path'])
@@ -28,47 +34,60 @@ async def handle_docs_photo(message: Message, state: FSMContext):
     await message.reply(data['path'], reply_markup=keyboard[data['path']])
 
 
-@dp.message_handler(state='*', commands=['start', 'help'])
-async def send_welcome(message: Message, state: FSMContext):
-
-    await state.finish()
-
-    async with state.proxy() as data:
-        data['path'] = '/'
-        if 'photo' not in data:
-            data['photo'] = {}
-    await message.reply("Введите адрес объекта", reply_markup=keyboard['/'])
-    await message.reply("init", reply_markup=ReplyKeyboardMarkup([['/start'], ['Назад'], ['Сохранить'], ['Перегрузить']]))
-
-
 @dp.message_handler(lambda message: message.text == 'Сохранить', state='*')
 async def save_progress(message: Message, state: FSMContext):
     async with state.proxy() as data:
+        if 'id' not in data:
+            await message.reply("Не выбран id задачи. Пришлите его в ответ")
+            return
+
         print(data.__dict__['_copy'])
-        with open('data/test.json', 'w') as f:
+        with open(os.path.join(BASEDIR, data['id'], 'data.json'), 'w') as f:
             json.dump(data.__dict__['_copy'], f)
     await message.reply('Прогресс сохранен')
 
-@dp.message_handler(lambda message: message.text == 'Перегрузить', state='*')
-async def load_progress(message: Message, state: FSMContext):
+
+@dp.message_handler(state='*', commands=['start', 'help'])
+async def start(message: Message, state: FSMContext):
+
     async with state.proxy() as data:
-        with open('data/test.json', 'r') as f:
-            ldata = json.load(f)
-            for key in ldata:
-                data[key] = ldata[key]
-    # print(type(data))
-    print(data)
-    await message.reply('Прогресс загружен с последнего сохранения')
+        if 'id' not in data:
+            await message.reply("Не выбран id задачи. Пришлите его в ответ")
+            return
+        data['path'] = '/'
+
+    await message.reply("Введите адрес объекта", reply_markup=keyboard['/'])
+    await message.reply("init", reply_markup=ReplyKeyboardMarkup([['/start'], ['Назад'], ['Сохранить'], ['Выход']]))
 
 
-@dp.message_handler(lambda message: message.text not in ['Назад', 'Сохранить', 'Главное меню', 'Перегрузить'], state='*')
+@dp.message_handler(lambda message: message.text == 'Выход', state='*')
+async def exit(message: Message, state: FSMContext):
+    await state.finish()
+    await message.reply('Работа завершена')
+
+
+@dp.message_handler(lambda message: message.text not in ['Назад', 'Сохранить', 'Главное меню', 'Выход'], state='*')
 async def value_saver(message: Message, state: FSMContext):
-    p_comm = '/Общие сведения/Площадь/Общая/'
-    p_live = '/Общие сведения/Площадь/Жилая/'
     async with state.proxy() as data:
-        if data['path'] in [p_comm, p_live]:
-            if not message.text.isdigit():
-                await message.reply(f'Введенно неверное значение, повторите еще раз')
+        if 'id' not in data:
+            if message.text.isdigit():
+                data['id'] = message.text
+                if not os.path.exists(os.path.join(BASEDIR, data['id'])):
+                    os.mkdir(os.path.join(BASEDIR, data['id']))
+                    os.mkdir(os.path.join(BASEDIR, data['id'], 'photo'))
+                else:
+                    path = os.path.join(BASEDIR, data['id'], 'data.json')
+                    if os.path.exists(path):
+                        with open(path, 'r') as f:
+                            ldata = json.load(f)
+                            for key in ldata:
+                                data[key] = ldata[key]
+                if 'photo' not in data:
+                    data['photo'] = {}
+                await message.reply(f"Выбран id задачи {message.text}")
+                return
+            else:
+                await message.reply("Не выбран id задачи. Пришлите его в ответ")
                 return
 
         put_key_json(data['path'], data, message.text)
@@ -78,9 +97,13 @@ async def value_saver(message: Message, state: FSMContext):
 
 
 @dp.message_handler(lambda message: message.text == 'Назад', state='*')
-async def bactrack(message: Message, state: FSMContext):
+async def backtrack(message: Message, state: FSMContext):
     path = ''
     async with state.proxy() as data:
+        if 'id' not in data:
+            await message.reply("Не выбран id задачи. Пришлите его в ответ")
+            return
+
         path = prev_path(data['path'])
         data['path'] = path
     await message.reply(data['path'], reply_markup=keyboard[path])
@@ -91,24 +114,9 @@ async def process_callback(callback_query: CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     path = ''
     async with state.proxy() as data:
-
-        if callback_query.data == 'Нежилая':
-            p_comm = '/Общие сведения/Площадь/Общая/'
-            p_live = '/Общие сведения/Площадь/Жилая/'
-            if p_comm in data and p_live in data:
-
-                area = calculate_area(data)
-                keyboard['/Общие сведения/Площадь/Нежилая/'] = get_keyboard(
-                    [f'рассчитано {area} м^2 правильно?'])
-                keyboard[f'/Общие сведения/Площадь/Нежилая/рассчитано {area} м^2 правильно?/'] = get_keyboard([
-                                                                                                              'Да', 'Нет'])
-                answer[f'рассчитано {area} м^2 правильно?/Нет'] = "Введите правильное значение"
-            else:
-                path = prev_path(data['path'])
-                data['path'] = path
-                msg = 'Значения для жилой и нежилой не введены'
-                await bot.send_message(callback_query.message.chat.id,  msg, reply_markup=keyboard[path])
-                return
+        if 'id' not in data:
+            await bot.send_message(callback_query.message.chat.id, "Не выбран id задачи. Пришлите его в ответ")
+            return
 
         path = data['path'] + callback_query.data + '/'
         for i in range(-3, 0):
